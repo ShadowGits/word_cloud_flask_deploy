79ca5e3da1a906d20fcd2f94dcc501db6ff4c446
ShadowGits <37514710+ShadowGits@users.noreply.github.com>
2020-07-03 17:34:59 +0530

    Adding wsgi

--- app.py
+++ app.py
@@ -1,34 +1,37 @@
 from flask import Flask, request, jsonify
 from getWordcloud import text_to_wordcloud
 #Init app
-app=Flask(__name__)
+
+def  create_app():
+    app=Flask(__name__)
 
 
     
 
 
-#GET IMAGE STRING
-@app.route("/Image",methods=["GET"])
-def get_image_string():
+    #GET IMAGE STRING
+    @app.route("/Image",methods=["GET"])
+    def get_image_string():
 
-    string_tuple=text_to_wordcloud("")
-    return jsonify({"image_string":string_tuple[0],
+        string_tuple=text_to_wordcloud("")
+        return jsonify({"image_string":string_tuple[0],
                     "image_string_invert":string_tuple[1]})
 
 
 
-#Customised IMAGE STRING
-@app.route("/Image",methods=["POST"])
-def get_text_image_string():
+    #Customised IMAGE STRING
+    @app.route("/Image",methods=["POST"])
+    def get_text_image_string():
 
-    text=request.json['input_text']
-    print(text)
-    string_tuple=text_to_wordcloud(text)
-    return jsonify({"image_string":string_tuple[0],
+        text=request.json['input_text']
+        print(text)
+        string_tuple=text_to_wordcloud(text)
+        return jsonify({"image_string":string_tuple[0],
                     "image_string_invert":string_tuple[1]})
+    return app
 
 
 #Run Server
 if __name__=="__main__":
-    app.run(debug=True)
+    create_app().run(debug=True)
     
