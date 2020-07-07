80dccd09938634ba4f95cf3f3ee0e2a8668ea157
ShadowGits <37514710+ShadowGits@users.noreply.github.com>
2020-07-07 23:04:54 +0530

    Custom Mask Commit 2

--- app.py
+++ app.py
@@ -6,9 +6,6 @@ def  create_app():
     app=Flask(__name__)
 
 
-    
-
-
     #GET IMAGE STRING
     @app.route("/Image",methods=["GET"])
     def get_image_string():
@@ -32,14 +29,14 @@ def  create_app():
                     "image_string_invert":string_tuple[1],
                     "image_string_masked":string_tuple[2],
                     "image_string_invert_masked":string_tuple[3]})
-    return app
 
     #Customised IMAGE MASK
     @app.route("/ImageWithMask",methods=["POST"])
     def get_text_image_string_with_custom_mask():
-
+        print("here 1")
         text=request.json['input_text']
         maskString=request.json['mask_string']
+        print("here 2")
         string_tuple=text_to_wordcloud_with_custom_mask(text,maskString)
         return jsonify({"image_string":string_tuple[0],
                     "image_string_invert":string_tuple[1],
